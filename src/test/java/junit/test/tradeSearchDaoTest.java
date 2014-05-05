c5015f3518e009064e88997cd7de35cd129656f7
HALOBING <656452024@qq.com>
2014-05-05 16:20:37 +0800

    add junit test

---
+++ src/test/java/junit/test/tradeSearchDaoTest.java
@@ -0,0 +1,17 @@
+package junit.test;
+
+import com.isoft.dao.TradeSearchDao;
+
+/**
+ * Created by admin on 14-5-5.
+ */
+
+public class TradeSearchDaoTest {
+
+    public void test(){
+        TradeSearchDao tsd=new TradeSearchDao();
+
+
+    }
+
+}
