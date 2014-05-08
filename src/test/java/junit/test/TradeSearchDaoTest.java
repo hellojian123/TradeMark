3f164bb6da94b3568f134d03e228940c0567edfe
HeJian <hellojian123@gmail.com>
2014-05-08 14:27:17 +0800

    Update TradeSearchDaoTest.java

--- src/test/java/junit/test/TradeSearchDaoTest.java
+++ src/test/java/junit/test/TradeSearchDaoTest.java
@@ -1,6 +1,5 @@
 package junit.test;
 
-import com.isoft.dao.TradeSearchDao;
 
 /**
  * Created by admin on 14-5-5.
@@ -9,8 +8,7 @@ import com.isoft.dao.TradeSearchDao;
 public class TradeSearchDaoTest {
 
     public void test(){
-        TradeSearchDao tsd=new TradeSearchDao();
-
+       
 
     }
 
